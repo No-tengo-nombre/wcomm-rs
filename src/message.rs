cfdd4be0a04d33d02f7479a239c93fbc41e0c629
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-18 16:06:05 -0400

    Fixed a bug

--- src/message.rs
+++ src/message.rs
@@ -1,71 +1,87 @@
+use std::fmt;
+
 pub struct Message {
     _data: String,
     _header: String,
+    _total: String,
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        return write!(f, "{}", self.as_string());
+    }
 }
 
 impl Message {
-    fn new() -> Message {
+    pub fn new() -> Message {
         return Message {
             _data: "".to_string(),
             _header: "".to_string(),
+            _total: "".to_string(),
         };
     }
 
-    fn from_string(data: &String, header: &String) -> Message {
+    pub fn from_string(data: &str, header: &str) -> Message {
         return Message {
-            _data: string_to_binary(data),
-            _header: string_to_binary(header),
+            _data: string_to_binary(&data),
+            _header: string_to_binary(&header),
+            _total: string_to_binary(&data) + &string_to_binary(&header),
         };
     }
 
-    fn from_binary(data: &String, header: &String) -> Message {
+    pub fn from_binary(data: &str, header: &str) -> Message {
         return Message {
             _data: data.to_string(),
             _header: header.to_string(),
+            _total: data.to_string() + &header.to_string(),
         };
     }
 
-    fn data(mut self, data: &String) -> Message {
-        self._data = string_to_binary(data);
+    pub fn data(mut self, data: &str) -> Message {
+        self._data = string_to_binary(&data);
+        self._total = self._header.clone() + &self._data;
         return self;
     }
 
-    fn data_from_binary(mut self, data: &String) -> Message {
+    pub fn data_from_binary(mut self, data: &str) -> Message {
         self._data = data.to_string();
+        self._total = self._header.clone() + &self._data;
         return self;
     }
 
-    fn header(mut self, header: &String) -> Message {
-        self._header = string_to_binary(header);
+    pub fn header(mut self, header: &str) -> Message {
+        self._header = string_to_binary(&header.to_string());
+        self._total = self._header.clone() + &self._data;
         return self;
     }
 
-    fn header_from_binary(mut self, header: &String) -> Message {
+    pub fn header_from_binary(mut self, header: &str) -> Message {
         self._header = header.to_string();
+        self._total = self._header.clone() + &self._data;
         return self;
     }
 
-    fn get_data(&self) -> String {
-        return self._data.clone();
+    pub fn get_data(&self) -> &str {
+        return &self._data;
     }
 
-    fn get_header(&self) -> String {
-        return self._header.clone();
+    pub fn get_header(&self) -> &str {
+        return &self._header;
     }
 
-    fn bit_size(&self) -> u32 {
-        return (self._header.len() + self._data.len()) as u32;
+    pub fn bit_size(&self) -> u32 {
+        return (self._total.len()) as u32;
     }
 
-    fn data_size(&self) -> u32 {
+    pub fn data_size(&self) -> u32 {
         return self._data.len() as u32;
     }
 
-    fn header_size(&self) -> u32 {
+    pub fn header_size(&self) -> u32 {
         return self._header.len() as u32;
     }
 
-    fn group_string(message: &String, num: u32) -> Vec<String> {
+    pub fn group_string(message: &String, num: u32) -> Vec<String> {
         let mut result = Vec::<String>::new();
         let mut temp = message.clone();
         while temp != "" {
@@ -75,19 +91,19 @@ impl Message {
         return result;
     }
 
-    fn group(&self, num: u32) -> Vec<String> {
-        return Message::group_string(&(self._header.clone() + &self._data), num);
+    pub fn group(&self, num: u32) -> Vec<String> {
+        return Message::group_string(&self._total, num);
     }
 
-    fn group_data(&self, num: u32) -> Vec<String> {
+    pub fn group_data(&self, num: u32) -> Vec<String> {
         return Message::group_string(&self._data, num);
     }
 
-    fn group_header(&self, num: u32) -> Vec<String> {
+    pub fn group_header(&self, num: u32) -> Vec<String> {
         return Message::group_string(&self._header, num);
     }
 
-    fn as_u8_array(&self) -> Vec<u8> {
+    pub fn as_u8_array(&self) -> Vec<u8> {
         let mut result = Vec::<u8>::new();
         for c in self.group(8) {
             result.push(isize::from_str_radix(&c, 2).unwrap().try_into().unwrap());
@@ -95,7 +111,7 @@ impl Message {
         return result;
     }
 
-    fn data_as_u8_array(&self) -> Vec<u8> {
+    pub fn data_as_u8_array(&self) -> Vec<u8> {
         let mut result = Vec::<u8>::new();
         for c in self.group_data(8) {
             result.push(isize::from_str_radix(&c, 2).unwrap().try_into().unwrap());
@@ -103,7 +119,7 @@ impl Message {
         return result;
     }
 
-    fn header_as_u8_array(&self) -> Vec<u8> {
+    pub fn header_as_u8_array(&self) -> Vec<u8> {
         let mut result = Vec::<u8>::new();
         for c in self.group_header(8) {
             result.push(isize::from_str_radix(&c, 2).unwrap().try_into().unwrap());
@@ -111,19 +127,19 @@ impl Message {
         return result;
     }
 
-    fn as_binary(&self) -> String {
-        return self._header.clone() + &self._data;
+    pub fn as_binary(&self) -> &str {
+        return &self._total;
     }
 
-    fn data_as_binary(&self) -> String {
-        return self._data.clone();
+    pub fn data_as_binary(&self) -> &str {
+        return &self._data;
     }
 
-    fn header_as_binary(&self) -> String {
-        return self._header.clone();
+    pub fn header_as_binary(&self) -> &str {
+        return &self._header;
     }
 
-    fn as_string(&self) -> String {
+    pub fn as_string(&self) -> String {
         let mut result = "".to_string();
         for c in self.as_u8_array() {
             result += &(c as char).to_string();
@@ -131,7 +147,7 @@ impl Message {
         return result;
     }
 
-    fn data_as_string(&self) -> String {
+    pub fn data_as_string(&self) -> String {
         let mut result = "".to_string();
         for c in self.data_as_u8_array() {
             result += &(c as char).to_string();
@@ -139,7 +155,7 @@ impl Message {
         return result;
     }
 
-    fn header_as_string(&self) -> String {
+    pub fn header_as_string(&self) -> String {
         let mut result = "".to_string();
         for c in self.header_as_u8_array() {
             result += &(c as char).to_string();
@@ -148,10 +164,10 @@ impl Message {
     }
 }
 
-pub fn string_to_binary(data: &String) -> String {
+pub fn string_to_binary(data: &str) -> String {
     let mut result = "".to_string();
     for c in data.bytes() {
-        result += &format!("0{c:b}");
+        result += &format!("{c:0>8b}");
     }
     return result;
 }
