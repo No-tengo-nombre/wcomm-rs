edb0c2ffb828c43e959db3ade4ce58512f25c22e
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-27 00:00:04 -0400

    Made msg a reference

--- src/channels/channel.rs
+++ src/channels/channel.rs
@@ -1,7 +1,7 @@
 use crate::Message;
 
 pub trait Channel {
-    fn send(&self, msg: Message, time: u32);
+    fn send(&self, msg: &Message, time: u32);
     fn play(&self, frequency: u32, time: u32);
     fn listen(&self, frequency: u32);
 }
