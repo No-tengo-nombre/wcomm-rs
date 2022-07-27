0e1c11c09a06280140c9ca64c9a7a12eedb0f2a0
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-27 19:46:23 -0400

    Added another modulation for the example

--- examples/wav_export/main.rs
+++ examples/wav_export/main.rs
@@ -3,9 +3,13 @@ use wcomm::modulation::MFSK;
 use wcomm::Message;
 
 fn main() {
-    let modulation = MFSK::new(256).base_frequency(100).delta_frequency(30);
-    let channel = Sound::new(&modulation);
-
     let msg = Message::new().data("Hello world!");
-    channel.export_wav(&msg, "examples/wav_export/audio/original.wav", 100);
+
+    let mod256 = MFSK::new(256).base_frequency(100).delta_frequency(30);
+    let channel256 = Sound::new(&mod256);
+    channel256.export_wav(&msg, "examples/wav_export/audio/original256.wav", 100);
+
+    let mod16 = MFSK::new(16).base_frequency(100).delta_frequency(100);
+    let channel16 = Sound::new(&mod16);
+    channel16.export_wav(&msg, "examples/wav_export/audio/original16.wav", 100);
 }
