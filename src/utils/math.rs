6b5151722e987b182a66b010a5137d9e8827ed68
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-27 23:09:26 -0400

    Added more functions to convert between gray and normal integers

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -9,8 +9,17 @@ pub fn log2(x: u32) -> u32 {
 }
 
 pub fn num_to_gray(num: u32) -> u32 {
-    let output = num_to_gray_binary(num);
-    return u32::from_str_radix(&output, 2).unwrap();
+    return num ^ (num >> 1);
+}
+
+pub fn gray_to_num(num: u32) -> u32 {
+    let mut result = num;
+    let mut mask = result;
+    while mask != 0 {
+        mask >>= 1;
+        result ^= mask;
+    }
+    return result;
 }
 
 pub fn num_to_gray_binary(num: u32) -> String {
