4ffe8d4129345508fe4f45074287756775c1fa85
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-28 21:32:37 -0400

    Made a sketch for the implementation of Huffman codes

--- src/encoding/source/huffman.rs
+++ src/encoding/source/huffman.rs
@@ -1,3 +1,37 @@
 use crate::encoding::source::SourceCoding;
+use crate::Message;
+use std::collections::HashMap;
 
-pub struct HuffmanCoding {}
+pub struct HuffmanCoding {
+    _code: HashMap<u8, String>,
+}
+
+impl SourceCoding for HuffmanCoding {
+    fn generate_code(msg: &Message) -> HashMap<u8, String> {
+        // TODO: Implement this
+        return HuffmanCoding::empty_char_map();
+    }
+
+    fn encode(&self, msg: &Message) -> Message {
+        // TODO: Implement this
+        return Message::new();
+    }
+
+    fn decode(&self, msg: &Message) -> Message {
+        // TODO: Implement this
+        return Message::new();
+    }
+}
+
+impl HuffmanCoding {
+    pub fn new() -> HuffmanCoding {
+        return HuffmanCoding {
+            _code: HuffmanCoding::empty_char_map(),
+        };
+    }
+
+    pub fn code(mut self, new_code: HashMap<u8, String>) -> HuffmanCoding {
+        self._code = new_code;
+        return self;
+    }
+}
