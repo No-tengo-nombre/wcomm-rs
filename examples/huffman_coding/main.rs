3e584e1daeeb3bfbfcca79eeb67c7f8876a1d4dd
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-08-02 01:08:40 -0400

    Made an example to test Huffman codes

--- examples/huffman_coding/main.rs
+++ examples/huffman_coding/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use wcomm::encoding::source::{HuffmanCoding, SourceCoding};
+use wcomm::Message;
+
+fn main() {
+    let small_msg = Message::from_string("Hello world!", "HEADER");
+    let encoder = HuffmanCoding::from_message(&small_msg);
+    let small_enc = encoder.encode(&small_msg);
+    let small_dec = encoder.decode(&small_enc, 0);
+    println!("{}", encoder);
+    println!(
+        "{} -> DECODED : {}",
+        // small_msg.as_binary(), small_dec.as_binary()
+        small_msg, small_dec
+    );
+}
