436dc520c78a0ca7ca640c4c90101a93d6d97a2e
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-08-10 00:12:20 -0400

    Made the general framework for channnel coding

--- src/encoding/channel/mod.rs
+++ src/encoding/channel/mod.rs
@@ -1,1 +1,31 @@
+mod hamming;
 
+use std::{error, fmt};
+
+pub use hamming::*;
+
+use crate::Message;
+
+#[derive(Debug)]
+pub struct ChannelError {
+    _bit_num: u32,
+}
+
+impl ChannelError {
+    pub fn new(num: u32) -> ChannelError {
+        return ChannelError { _bit_num: num };
+    }
+}
+
+impl fmt::Display for ChannelError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        return write!(f, "Encountered {} wrong bits.", self._bit_num);
+    }
+}
+
+impl error::Error for ChannelError {}
+
+pub trait ChannelCoding {
+    fn encode(msg: &Message) -> Message;
+    fn decode(msg: &Message) -> Result<Message, ChannelError>;
+}
